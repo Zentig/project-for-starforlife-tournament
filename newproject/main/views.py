5432a81f3304dd1e15c0d03c584bb6be2776137f
Zentig <hhea7en@gmail.com>
2026-04-01 20:13:17 +0300

    Added some design

--- newproject/main/views.py
+++ newproject/main/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html',
+                  {"title": "Головна сторінка"})
 
 def about(request):
     return render(request, 'main/about.html')
