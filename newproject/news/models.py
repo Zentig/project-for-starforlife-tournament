d21aabb6abac3a26d8d845ad249d46cf151208df
Zentig <hhea7en@gmail.com>
2026-04-03 23:13:40 +0300

    Added migrations to the project, progressed with an admin panel

--- newproject/news/models.py
+++ newproject/news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    # title, announcement, full_text, date
+    title = models.CharField('Назва новини', max_length=50)
+    announcement = models.CharField('Анонс', max_length=250)
+    full_text = models.TextField('Повний текст статті')
+    date = models.DateTimeField('Дата публікації')
