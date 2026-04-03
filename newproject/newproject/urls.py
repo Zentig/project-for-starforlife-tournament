f2e56d141806b11bdae7f6a8d4f547a1b9359029
Zentig <hhea7en@gmail.com>
2026-04-03 23:02:12 +0300

    Preparation to test databases

--- newproject/newproject/urls.py
+++ newproject/newproject/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('', include("main.urls")),
     path('admin/', admin.site.urls),
+    path('news/', include("news.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
